dd0d9c0af5bdcc52f2552ac39ec8ba91f47c3043
Tristan Downing <downing.tristan@gmail.com>
2025-06-02 16:14:59 -0700

    logging, chirps gefs

--- pipelines/update_chirps_gefs.py
+++ pipelines/update_chirps_gefs.py
@@ -2,7 +2,13 @@ from src.datasources.chirps_gefs import (
     download_recent_chirps_gefs,
     process_recent_chirps_gefs,
 )
+from src.utils.logging import get_logger
+
+logger = get_logger(__name__)
 
 if __name__ == "__main__":
+    logger.info("Starting the CHIRPS-GEFS update pipeline...")
+    logger.info("Downloading recent CHIRPS-GEFS data...")
     download_recent_chirps_gefs()
+    logger.info("Processing recent CHIRPS-GEFS data...")
     process_recent_chirps_gefs()
